0c06194a28086b776f31235b6e0972358aa338a0
Rens Admiraal <rens@rensadmiraal.nl>
2014-11-01 16:55:00 +0200

    [TASK] Add grunt config

--- Build/Gruntfile.js
+++ Build/Gruntfile.js
@@ -3,21 +3,80 @@ module.exports = function (grunt) {
 
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-		compass: grunt.file.readJSON('../../../../Build/Grunt/Defaults/compass.json'),
-		watch: grunt.file.readJSON('../../../../Build/Grunt/Defaults/watch.json'),
-		requirejs: grunt.file.readJSON('../../../../Build/Grunt/Defaults/requirejs.json'),
-		concat: grunt.file.readJSON('../../../../Build/Grunt/Defaults/concat.json'),
+		compass: {
+			"dev": {
+				"options": {
+					"sassDir": "../Resources/Private/Styles/",
+					"cssDir": "../Resources/Public/Styles/",
+					"environment": "development",
+					"outputStyle": "nested",
+					"bundleExec": true
+				}
+			},
+			"dist": {
+				"options": {
+					"sassDir": "../Resources/Private/Styles/",
+					"cssDir": "../Resources/Public/Styles/",
+					"environment": "production",
+					"outputStyle": "compressed",
+					"bundleExec": true
+				}
+			}
+		},
+		watch: {
+			"css": {
+				"files": "../Resources/Private/Styles/{,*/}*.{scss,sass}",
+				"tasks": ["compass:dev"],
+				"options": {
+					"spawn": false,
+					"debounceDelay": 250,
+					"interrupt": true,
+					"bundleExec": true
+				}
+			},
+			"scripts": {
+				"files": "../Resources/Private/Scripts/**/*.*",
+				"tasks": ["concat"],
+				"options": {
+					"spawn": false,
+					"debounceDelay": 250,
+					"interrupt": true,
+					"bundleExec": true
+				}
+			}
+		},
+		requirejs: {
+			"compile": {
+				"options": {
+					"baseUrl": "../../../../Web/_Resources/Static/Packages/",
+					"paths": {
+						"Library": "SociaalIntranet.Boilerplate/Library/",
+						"text": "SociaalIntranet.Boilerplate/Library/text"
+					},
+					"name": "SociaalIntranet.Boilerplate/Scripts/Site",
+					"out": "../Resources/Public/Scripts/Site.js"
+				}
+			}
+		},
+		concat: {
+			"mirrorApp": {
+				"expand": true,
+				"cwd": "../Resources/Private/Scripts/",
+				"src": ["**/*.js", "**/*.html"],
+				"dest": "../Resources/Public/Scripts/"
+			},
+			"mirrorLibrary": {
+				"expand": true,
+				"cwd": "dist/library/",
+				"src": ["**/*.js"],
+				"dest": "../Resources/Public/Library/"
+			}
+		},
 		clean: {
 			options: {
 				force: true
 			},
 			js: ["../Resources/Public/Scripts/**/*.js"]
-		}
-	});
-
-	grunt.extendConfig({
-		concat: {
-			// Libraries
 		}
 	});
 
